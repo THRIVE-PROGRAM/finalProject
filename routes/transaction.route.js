89a5d23576a5fcaf1c65e9810f710ab6dde17959
denzonn <denson753@gmail.com>
2024-05-30 18:32:59 +0800

    Fixing

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,10 +1,8 @@
 const authentication = require('../middlewares/authentication')
 const router = require('../helpers/router');
 const { createTransaction, getTransaction } = require('../controllers/transaction.controller');
-const { buyerAuthorization, sellerAuthorization } = require('../middlewares/authorization');
 
 router.use(authentication)
-router.use(buyerAuthorization, sellerAuthorization)
 router.get('/get-transaction', getTransaction)
 router.post('/create-transaction', createTransaction)
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -6,15 +6,10 @@ const {
 } = require("../controllers/user.controller");
 const router = require("../helpers/router");
 const authentication = require("../middlewares/authentication");
-const {
-  buyerAuthorization,
-  sellerAuthorization,
-} = require("../middlewares/authorization");
 
 router.post("/register", register);
 router.post("/signIn", signIn);
 router.use(authentication);
-router.use(buyerAuthorization, sellerAuthorization);
 router.get("/get/:id", getUser);
 router.put("/update/:id", updateUser);
 
